f614d667a49665879128787149afbbffad360091
strelec00 <jan.strelec853@gmail.com>
2023-01-07 23:25:20 +0100

    popravak
    main

--- includes/main.php
+++ includes/main.php
@@ -3,28 +3,28 @@
     <div class="t-kolona-12 d-kolona-4">
         <h4>O nama</h4>
         <ul>
-                    <li>1 Nudi vrhunske usluge iz različitih područja medicine</li>
+                    <li>Nudimo vrhunske usluge iz različitih područja medicine</li>
                     <br>
-                    <li>2 Mogučnost kartičnog plaćanja</li>
+                    <li>Nudi ostanak i pruža pomoć u oporavku nakon operacije</li>
                     <br>
-                    <li>3 Nudi ostanak i pruža pomoć u oporavku nakon operacije</li>
+                    <li>Mogučnost kartičnog plaćanja</li>
                     <br>
                 </ul>
     </div>
     <div class="t-kolona-12 d-kolona-4" id="main_drugi">
         <h4>Kontakt</h4>
                 <ul>
-                    <li>1</li>
-                    <li>2</li>
-                    <li>3</li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
                 </ul>
     </div>
     <div class="t-kolona-12 d-kolona-4" id="main_treci">
         <h4>Pomoc</h4>
         <ul>
-                    <li>1</li>
-                    <li>2</li>
-                    <li>3</li>
+                    <li></li>
+                    <li></li>
+                    <li> </li>
                 </ul>
     </div>
    
