f0cb481777d757ab3a676271e8518d7410c4719f
strelec00 <jan.strelec853@gmail.com>
2023-01-11 14:04:56 +0100

    razmak izmedu liste na pocetnoj
    razmaci izmedu liste su bili premali, pa sam ih poveco

--- includes/main.php
+++ includes/main.php
@@ -3,12 +3,16 @@
     <div class="t-kolona-12 d-kolona-3" id="izbornik_blok">
         <h4>O nama</h4>
         <ul>
-                    <li>Nudimo vrhunske usluge iz različitih područja medicine</li>
+                    <!-- <li>Nudimo vrhunske usluge iz različitih područja medicine</li>
                     <br>
                     <li>Nudi ostanak i pruža pomoć u oporavku nakon operacije</li>
                     <br>
                     <li>Mogučnost kartičnog plaćanja</li>
-                    <br>
+                    <br> -->
+                    <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry</li>
+                    <li>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </li>
+                    <li>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </li>
+                    <li>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </li>
                 </ul>
     </div>
 
@@ -17,9 +21,10 @@
     <div class="t-kolona-12 d-kolona-3" id="izbornik_blok">
         <h4>Kontakt</h4>
                 <ul>
-                    <li>Mogučnost kartičnog  kartičnog kartičnog kartičnog kartičnog plaćanjaMogučnost kartičnog plaćanjaMogučnost kartičnog plaćanjaMogučnost kartičnog plaćanja</li>
-                    <li></li>
-                    <li></li>
+                <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry</li>
+                    <li>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </li>
+                    <li>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </li>
+                    <li>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </li>
                 </ul>
     </div>
     <div class="t-kolona-12 d-kolona-1-5">
@@ -27,9 +32,10 @@
     <div class="t-kolona-12 d-kolona-3" id="izbornik_blok">
         <h4>Pomoc</h4>
         <ul>
-                    <li></li>
-                    <li></li>
-                    <li> </li>
+        <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry</li>
+                    <li>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </li>
+                    <li>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </li>
+                    <li>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </li>
                 </ul>
     </div>
    
