c88fe3b91dba0a424d31d6a17fc1508c587eab64
strelec00 <jan.strelec853@gmail.com>
2023-01-16 15:16:24 +0100

    hamburger meni
    napravil hamburger meni i obrisal ono kaj nije trebalo

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,12 +1,12 @@
 <div class="d-kolona-1 t-kolona-0">
     
     </div>
-    <div id="logo" class="d-kolona-2 t-kolona-3">
+    <div id="logo" class="d-kolona-2 t-kolona-5">
         <a href="index.php">
             <img src="./slike/logo2.png" />
         </a>
 </div>
-<nav id="navigacija" class="d-kolona-9 t-kolona-9">		
+<nav id="navigacija" class="d-kolona-9 t-kolona-8">
     
     <ul>
         <li><a href="index.php">POČETNA</a></li>
@@ -16,6 +16,25 @@
         <li><a href="kontakt.php">KONTAKT</a></li>     
         <li><a href="login.php">
     <button class="button-85" role="button">Login</button>
+    
     </a></li>
-    </ul>
+</ul>
 </nav>
+<button class="hamburger">
+            <div class="bar"></div>
+        </button>
+	<div class="mobile-nav">
+    <ul>
+        <a href="index.php">POČETNA</a>
+        <a href="termini.php">TERMINI</a>
+        <a href="pacijenti.php">PACIJENTI</a>
+        <a href="onama.php">O NAMA</a>
+        <a href="kontakt.php">KONTAKT</a>    
+        <a href="login.php">
+LOGIN    
+    </a></li>
+</ul>
+    </div>
+    <script src="./js/hamburger.js"></script>
+
+
